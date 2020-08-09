aa3c63196f01e59289cb0ad4289614d6df6f29a6
luuthikieuoanh <67403607+luuthikieuoanh@users.noreply.github.com>
2020-08-09 22:31:19 +0700

    ahihi

--- java1/src/QLSV/QLSVController.java
+++ java1/src/QLSV/QLSVController.java
@@ -1,7 +1,7 @@
 package QLSV;
 
 import java.awt.Color;
-import java.awt.Rectangle;
+import java.text.ParseException;
 import javax.swing.border.Border;
 
 import java.text.SimpleDateFormat;
@@ -9,8 +9,9 @@ import java.util.Date;
 import java.util.LinkedList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.swing.BorderFactory;
-import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
 public class QLSVController {
@@ -74,11 +75,7 @@ public class QLSVController {
                     //view.setTxtAlert("Error: bạn đã nhập sai!!!", Color.red);
                     return;
                 }
-                if (model.CheckTrungMa(model, sv.getMaSV())) {
-                    System.out.println("trùng mã");
-                    //view.setTxtAlert("Error: trùng mã!!!", Color.red);
-                    return;
-                }
+
                 model.add(sv);
                 ShowTable(model.getDssv());
             } catch (Exception ex) {
@@ -117,7 +114,7 @@ public class QLSVController {
             SimpleDateFormat ft = new SimpleDateFormat("dd-MM-yyyy");
             table.addRow(new Object[]{sv.getMaSV(), sv.getHoTen(), sv.getGioiTinh(),
                 ft.format(sv.getNgaySinh()), sv.getKhoa(), sv.getLop(),
-                sv.getEmail(), sv.getsDT(), sv.getDiaChi()});
+                sv.getDiaChi(), sv.getsDT(), sv.getEmail()});
         });
 
         view.getTb_SV().setModel(table);
@@ -126,6 +123,7 @@ public class QLSVController {
     //Thêm sinh viên
     public SinhVien AddSV(QLSVView view) {
         Border nhapSai = BorderFactory.createMatteBorder(1, 1, 1, 1, Color.red);
+          Border nhapDung = BorderFactory.createMatteBorder(1, 1, 1, 1, Color.BLACK);
         String maSV = view.getTxt_msv().getText();
         String tenSV = view.getTxt_hoTen().getText();
         String gioiTinh = view.isGioiTinhNam() ? "Nam" : "Nu";
@@ -135,7 +133,7 @@ public class QLSVController {
         String khoa = view.getTxt_Khoa().getText();
         String lop = view.getTxt_Lop().getText();
         String sDT = view.getTxt_SDT().getText();
-        
+
         //điều kiện-------------
         if (maSV.equals("")) {
             //code chỉnh border
@@ -143,17 +141,83 @@ public class QLSVController {
         }
         if (tenSV.equals("")) {
             view.getTxt_hoTen().setBorder(nhapSai);
-        }if (view.getGrGioiTInh().getSelection() == null) {
-            
+        }
+        //NgaySinh
+        if (ngaySinh.equals("") || checkNgaySinh(ngaySinh) == false) {
+            view.getTxt_NS().setBorder(nhapSai);
+        }
+        if (diaChi.equals("")) {
+            view.getTxt_DC().setBorder(nhapSai);
+        }
+        if ("".equals(email)) {
+            view.getTxt_Email().setText("");
+        } else {
+            if (validate_email(email) == false) {
+                view.getTxt_Email().setBorder(nhapSai);
+            }
+        }
+        if (khoa.equals("")) {
+            view.getTxt_Khoa().setBorder(nhapSai);
+        }
+        if (lop.equals("")) {
+            view.getTxt_Lop().setBorder(nhapSai);
+        }
+        if (!validate_SDT(sDT) || (sDT.length() != 10 && sDT.length() != 11)) {
+            view.getTxt_SDT().setBorder(nhapSai);
+        }
+        if (model.CheckTrungMa(model, maSV)) {
+            view.getTxt_msv().setBorder(nhapSai);
+
         }
         //ghi code vô đây
         //
-        
-        if (!maSV.equals("") && !tenSV.equals("")) {
+        boolean bl1 = maSV.equals("") || tenSV.equals("") || ngaySinh.equals("") || checkNgaySinh(ngaySinh) == false || diaChi.equals("") || khoa.equals("") || lop.equals("");
+        boolean bl2 = validate_SDT(sDT) == false || (sDT.length() != 10 && sDT.length() != 11) || model.CheckTrungMa(model, maSV);
+        if (!(bl1 || bl2)) {
             Date d = new Date(ngaySinh);
+
             return new SinhVien(maSV, tenSV, gioiTinh, d, khoa, lop, email, sDT, diaChi);
+
         } else {
             return null;
+        }
+    }
+
+    private void setSuccess() {
+        view.getTxt_msv().setText("");
+        view.getTxt_DC().setText("");
+        view.getTxt_Email().setText("");
+        view.getTxt_Khoa().setText("");
+        view.getTxt_Lop().setText("");
+        view.getTxt_NS().setText("");
+        view.getTxt_SDT().setText("");
+        view.getTxt_Tim().setText("");
+        view.getTxt_hoTen().setText("");
+        
+    }
+
+    private static final Pattern VALID_SDT_REGEX
+            = Pattern.compile("^[0-9]+$", Pattern.CASE_INSENSITIVE);
+
+    private static final Pattern VALID_EMAIL_ADDRESS_REGEX
+            = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+    public static boolean validate_email(String emailStr) {
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(emailStr);
+        return matcher.find();
+    }
+
+    public static boolean validate_SDT(String sDT) {
+        Matcher matcher = VALID_SDT_REGEX.matcher(sDT);
+        return matcher.find();
+    }
+
+    public static boolean checkNgaySinh(String ngaySinh) {
+        try {
+            Date d = new Date(ngaySinh);
+            return true;
+        } catch (Exception e) {
+            return false;
         }
     }
 
